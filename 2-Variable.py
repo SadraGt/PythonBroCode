ae953a1eec015298e760261c2c5034aa4264847f
Sadra <123418962+SadraGt@users.noreply.github.com>
2025-01-18 18:08:35 +0330

    Add files via upload

---
+++ 2-Variable.py
@@ -0,0 +1,13 @@
+#Variable --> String , Integer, Float, Boolean
+
+Name = "Sadra"
+Age = 21
+Weight = 62.5
+Student = True
+
+print(f"hi {Name} you are {Age} years old and Weight is :{Weight} kg")
+
+if Student:
+    print(f"you are student")
+else:
+    print("your not a student")
