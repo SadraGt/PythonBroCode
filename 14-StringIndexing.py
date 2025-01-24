630529de162033de70c4d324ab71925fbe86e357
Sadra <123418962+SadraGt@users.noreply.github.com>
2025-01-24 22:39:33 +0330

    Add files via upload

--- 14-StringIndexing.py
+++ 14-StringIndexing.py
@@ -7,4 +7,14 @@ X = "1234-5678-9012-3456"
 # print(X[5:9]) #5678
 # print(X[5:])  #5678-9012-3456
 # print(X[-1])  #6
-print(X[::3])
+# print(X[::3])
+
+# Delete site domain
+SiteName = input("Enter sait name:")
+
+A = (len(SiteName) - SiteName.rfind(".") ) * (-1 ) 
+if SiteName.rfind(".") == -1:
+    print("Error")
+else:
+    print(SiteName[:A])
+
