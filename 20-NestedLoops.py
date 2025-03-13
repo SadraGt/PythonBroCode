71ffefcb3c31336f43e47a3ed720e2efbeb27a97
Sadra <123418962+SadraGt@users.noreply.github.com>
2025-03-13 18:13:44 +0330

    Add files via upload

---
+++ 20-NestedLoops.py
@@ -0,0 +1,8 @@
+ro = int(input("Enter rows : "))
+co = int(input("Enter columns : "))
+ch = input("Enter a @#$%...:")
+for i in range(ro):
+    for x in range(co):
+        print(ch, end="")
+    print()
+
