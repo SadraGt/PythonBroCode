76fb72a0aeaf6060792d7858a26cb0986c68f922
Sadra <123418962+SadraGt@users.noreply.github.com>
2025-03-13 18:11:22 +0330

    Add files via upload

---
+++ 15-FormatSpacifiers.py
@@ -0,0 +1,11 @@
+# {value:flags}
+x1 = 1000.2525
+
+print(f"test : {x1:<20}")
+print(f"test : {x1:>20}")
+print(f"test : {x1:^20}")
+print(f"test : {x1:+}")
+print(f"test : {x1: }")
+print(f"test : {x1:.2f}")
+print(f"test : {x1:.10f}")
+print(f"test : {x1:,}")
