225771b0cfd182061de4044f5f7257265fb12fae
Sadra <123418962+SadraGt@users.noreply.github.com>
2025-03-17 11:46:05 +0330

    Add files via upload

---
+++ 35-Iterables.py
@@ -0,0 +1,26 @@
+# numbers = (1, 2, 3, 4)
+# numbers = [1, 2, 3, 4]
+
+# for num in numbers:
+#     print(num)
+
+#-------------------------------------
+
+# fruits = {"apple", "orange", "banana", "coconut"}
+
+# for fruit in fruits:
+#     print(fruit)
+
+#-------------------------------------
+
+# name = "Bro Code"
+
+# for character in name:
+#     print(character, end=" ")
+
+#-------------------------------------
+
+my_dictionary = {"A":1, "B":2, "C":3}
+
+for x, n in my_dictionary.items():
+    print(f"{x}:{n}")
