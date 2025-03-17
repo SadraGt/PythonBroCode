d4353dfe0ccc3b318fab78c3b4b40b8ac45b7eff
Sadra <123418962+SadraGt@users.noreply.github.com>
2025-03-17 11:46:30 +0330

    Add files via upload

---
+++ 36-MembershipOperators.py
@@ -0,0 +1,14 @@
+# grades = {"Sandy": "A",
+#           "Squidward":"B",
+#           "Spongebob":"C",
+#           "Patrick": "D"}
+
+# student = input("Enter student name : ")
+
+# if student in grades:
+#     print(grades[student])
+
+email = input("Enter email : ")
+
+if  "@" not in email and "." not in email:
+    print("not email.")
