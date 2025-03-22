77487b86e0d571ce7cce325c928d8df662a47d34
Sadra <123418962+SadraGt@users.noreply.github.com>
2025-03-22 16:21:53 +0330

    Add files via upload

---
+++ Li41ifname1.py
@@ -0,0 +1,11 @@
+
+def f_food(food):
+    print(f"your favorite food is {food}")
+
+def main():
+    print("this is script1")
+    f_food("pizza")
+    print("Gooodbye")
+
+if __name__ == '__main__':
+    main()
---
+++ Li41ifname2.py
@@ -0,0 +1,13 @@
+from Li41ifname1 import *
+
+def f_drink(drink):
+    print(f"your favorite drink is {drink}")
+
+def main():
+    print("this is script2")
+    f_food("sushi")
+    f_drink("coffee")
+    print("goodbye")
+
+if __name__ == '__main__':
+    main()
